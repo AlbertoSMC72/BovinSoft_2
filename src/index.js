02ded99e080d2a682c3a9ff9f34b3c45a92869b8
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2023-10-20 04:51:02 -0600

    deleted:    src/atomos/botonGenerico.jsx 	deleted:    src/atomos/entradaTexto.jsx 	deleted:    src/atomos/logoSimple.jsx 	deleted:    src/atomos/textoExplicativo.jsx 	deleted:    src/atomos/textoGenerico.jsx 	deleted:    src/atomos/textoGrande.jsx 	deleted:    src/css/colorNav.css 	deleted:    src/css/formCrearCuenta.css 	deleted:    src/img/logoA.png 	modified:   src/index.js 	deleted:    src/moleculas/logo_Nombre.jsx 	deleted:    src/organismos/formCrearCuenta.jsx 	deleted:    src/organismos/navbarSimple.jsx 	deleted:    src/pages/crearCuentaVista.jsx

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import VistaCrearCuenta from './pages/crearCuentaVista.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
     <div>
-      <VistaCrearCuenta />
     </div>
 
   </React.StrictMode>
